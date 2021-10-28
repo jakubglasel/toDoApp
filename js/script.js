10bdfbf86e77b5adb1c1112eab8446b801479292
Jakub Glasel <jakub.glasel@gmail.com>
2021-10-28 10:37:05 +0100

    localstorage checkerUpdater added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-const body = document.querySelector('body');
+const dataBase = JSON.parse(window.localStorage.getItem('db'));
 
+/*
+Function checks if local DB exists.
+If is not created already, creates default empyt one.
+If DB exists, updates it.
+*/
 
-console.log(body)
+function storageCheckerUpdater (string = 'db')
+{
+  localStorage = window.localStorage;
+  if (localStorage.getItem(string)) {
+    localStorage.setItem('db', JSON.stringify(dataBase));
+  } else {
+    localStorage.setItem(string, '{"work" : [],"home" : [] ,"hobby" : [],"study" : []}');
+  }
+}
+
+storageCheckerUpdater();
+
+// function braketsRemover (data)
+// {
+//     const key = Object.keys(data)[0]
+//     const parsedFirstBr = data[key].replace('[', '')
+//     const parsedSecondBr = parsedFirstBr.replace(']', '')
+//     const cleanData = parsedSecondBr.split(',')
+//     return cleanData
+// }
+
+console.log(dataBase.home[1]);
