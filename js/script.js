1b0f091161c903becbd119924084f5fa48551328
Jakub Glasel <jakub.glasel@gmail.com>
2021-10-28 17:40:19 +0100

    chnaged nav bar, added grid to display notes, chnages in js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-const dataBase = JSON.parse(window.localStorage.getItem('db'));
+const { localStorage } = window;
+let dataBase;
+
+function storageCheckerUpdater (string = 'db')
+{
+  if (localStorage.getItem('db')) {
+    if (dataBase === undefined) {
+      dataBase = JSON.parse(localStorage.getItem(string));
+    }
+    localStorage.setItem(string, JSON.stringify(dataBase));
+  } else {
+    localStorage.setItem(string, '{"work" : [],"home" : [] ,"hobby" : [],"study" : []}');
+    dataBase = JSON.parse(localStorage.getItem(string));
+  }
+}
 
 /*
 Function checks if local DB exists.
@@ -6,14 +20,51 @@ If is not created already, creates default empyt one.
 If DB exists, updates it.
 */
 
-function storageCheckerUpdater (string = 'db')
+function updateDom (category = 'work')
 {
-  localStorage = window.localStorage;
-  if (localStorage.getItem(string)) {
-    localStorage.setItem('db', JSON.stringify(dataBase));
-  } else {
-    localStorage.setItem(string, '{"work" : [],"home" : [] ,"hobby" : [],"study" : []}');
-  }
+  document.querySelector('ul').innerHTML = '';
+  /* index set to rotate note colors version */
+  let index = 1;
+
+  dataBase[`${ category }`].forEach((element) =>
+  {
+    if (index > 4) {
+      index = 1;
+    }
+    const li = document.createElement('li');
+    li.innerHTML = `${ element }`;
+    li.classList = `note--color${ index }`;
+    document.querySelector('ul').appendChild(li);
+
+    index += 1;
+  });
 }
+
+const fromBtn = document.querySelector('.form__btn');
+const category = document.querySelector('.category');
+const note = document.getElementById('note');
+const navBtn = document.querySelectorAll('.nav__btn');
+
+/*
+eventlistener for adding new notes to correct arrays in DB
+*/
+
+navBtn.forEach((element) =>
+{
+  element.addEventListener('click', () =>
+  {
+    updateDom(element.innerHTML);
+  });
+});
+
+fromBtn.addEventListener('click', (e) =>
+{
+  e.preventDefault();
+  dataBase[category.value].push(note.value);
+  updateDom(category.value);
+  category.value = 'default';
+  note.value = '';
+  storageCheckerUpdater();
+});
 
 storageCheckerUpdater();
